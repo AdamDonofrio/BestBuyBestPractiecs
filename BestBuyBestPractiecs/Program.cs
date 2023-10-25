8953815ae0e044e356dcbc924ae8428dc4e25cb6
unknown <donofrio.8@wright.edu>
2023-10-25 12:04:03 -0400

    message

--- BestBuyBestPractiecs/Program.cs
+++ BestBuyBestPractiecs/Program.cs
@@ -1,2 +1,62 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+
+
+using BestBuyBestPractiecs;
+using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+var config = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json")
+    .Build();
+
+
+string connString = config.GetConnectionString("DefaultConnection");
+
+IDbConnection conn = new MySqlConnection(connString);
+
+#region Department Section
+/*
+var departmentRepo = new DapperDepartmentRepository(conn);
+
+var departments = departmentRepo.GetAllDepartments();
+
+foreach (var department in departments)
+{
+    Console.WriteLine(department.DepartmentID);
+    Console.WriteLine(department.Name);
+    Console.WriteLine();
+    Console.WriteLine();
+}*/
+#endregion
+
+var productRepository = new DapperProductRepository(conn);
+
+/*var productToUpdate = productRepository.GetProduct(940);
+
+productToUpdate.Name = "UPDATED!";
+productToUpdate.Price = 12.99;
+productToUpdate.CategoryID = 1;
+productToUpdate.OnSale = false;
+productToUpdate.StockLevel = 1000;
+
+productRepository.UpdateProduct(productToUpdate);*/
+
+productRepository.DeleteProduct(940);
+
+var products = productRepository.GetAllProducts();
+
+foreach (var product in products)
+{
+    Console.WriteLine(product.ProductID);
+    Console.WriteLine(product.Name);
+    Console.WriteLine(product.Price);
+    Console.WriteLine(product.CategoryID);
+    Console.WriteLine(product.OnSale);
+
+    Console.WriteLine();
+    Console.WriteLine();
+}
+
